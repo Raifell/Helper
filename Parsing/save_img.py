30af191b11d9e241ccee3e05c33aec44d9357b41
Raifell <ArtSkvaer2@gmail.comm>
2023-07-20 16:54:42 +0500

    add comment

--- Parsing/save_img.py
+++ Parsing/save_img.py
@@ -2,5 +2,5 @@ import requests
 
 im_data = requests.get('https://www.parsemachine.com/media/cache/90/ee/90eee0c6163a13b739ac70ffb6e7d65a.png')
 
-with open('image_1.png', 'wb') as file:
+with open('image_1.png', 'wb') as file: # Запись в байтовом формате 'wb'
     file.write(im_data.content) # По ссылке скачаиваем данные параметра .content
