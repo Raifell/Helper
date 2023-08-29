47e289c2440cace4c27b0ce1e7d1bc102ba61a49
Raifell <ArtSkvaer2@gmail.comm>
2023-08-30 01:22:06 +0500

    add Algorithms new list

--- Algorithms/dec_list.py
+++ Algorithms/dec_list.py
@@ -4,3 +4,6 @@ myList=[[[1, 2, 3], [4, 5, 6]], [[2, 3, 4], [3, 4, 5]], [[4, 6, 7], [5, 7, 9]]]
 print(reduce(lambda x, y: x+y, myList))
 
 result = [[1, 2, 3], [4, 5, 6], [2, 3, 4], [3, 4, 5], [4, 6, 7], [5, 7, 9]]
+
+a = [[[x for x in range(5)] for _ in range(3)] for _ in range(2)]
+
