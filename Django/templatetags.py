888e3d8766e46d3fea09439ab698f28f95bcf5ff
Raifell <136842990+Raifell@users.noreply.github.com>
2023-11-18 18:01:54 +0500

    Create templatetags.py
    create simpletags

---
+++ Django/templatetags.py
@@ -0,0 +1,32 @@
+# temlatetags/tag_name
+
+from django import template
+from main.models import Category
+
+register = template.Library()
+
+
+@register.simple_tag()
+def get_category():
+    return Category.objects.all()
+
+# settings
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'libraries': {
+                'tag_name': 'app_name.templatetags.tag_name'
+            }
+        },
+    },
+]
